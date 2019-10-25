3f2f1eb6a3a096069516352e9b0c5a9ba41bbd8b
zhousg <zhoushugang@itcast.cn>
2019-10-25 12:12:20 +0800

    登录-路由配置

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    hm-toutiao-pc-84 <el-button type="success">成功按钮</el-button>
+    <router-view />
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,14 @@
 import VueRouter from 'vue-router'
 import Vue from 'vue'
+
+import Login from '@/views/login'
+
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-  routes: []
+  routes: [
+    { path: '/login', component: Login }
+  ]
 })
 
 export default router
