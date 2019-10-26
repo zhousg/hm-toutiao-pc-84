d3f17ffacd7834ab32202f5daa5228a088202f01
zhousg <zhoushugang@itcast.cn>
2019-10-26 12:24:51 +0800

    首页布局&折叠效果&路由开启&404处理

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,27 @@ import VueRouter from 'vue-router'
 import Vue from 'vue'
 
 import Login from '@/views/login'
+import Home from '@/views/home'
+import Welcome from '@/views/welcome'
+import NotFound from '@/views/404'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [
-    { path: '/login', component: Login }
+    // 登录
+    { path: '/login', component: Login },
+    // 首页
+    {
+      path: '/',
+      component: Home,
+      children: [
+        // 欢迎
+        { path: '/', component: Welcome }
+      ]
+    },
+    // 匹配  不符合路由规则的路径
+    { path: '*', component: NotFound }
   ]
 })
 
