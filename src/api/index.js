bda935ba438d48776eda9874f66e69d4590d518e
zhousg <zhoushugang@itcast.cn>
2019-11-01 10:51:21 +0800

    频道组件封装

--- src/api/index.js
+++ src/api/index.js
@@ -2,10 +2,20 @@
 import axios from 'axios'
 import local from '@/utils/local'
 import router from '@/router'
+import JSONBIG from 'json-bigint'
 
 // 对axios进行配置
 // 基准地址
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
+axios.defaults.transformResponse = [(data) => {
+  // 对data进行格式转换  data就是后台响应的json字符串
+  // 如果没数据呢？data === null 使用JSONBIG.parse(null) 报错
+  try {
+    return JSONBIG.parse(data)
+  } catch (e) {
+    return data
+  }
+}]
 // 设置默认的请求头
 // if (local.getUser()) {
 //   axios.defaults.headers.Authorization = `Bearer ${local.getUser().token}`
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 import MyBread from '@/components/my-bread'
+import MyChannel from '@/components/my-channel'
 export default {
   install (Vue) {
     // Vue 是一个构造函数
     Vue.component('my-bread', MyBread)
+    Vue.component('my-channel', MyChannel)
   }
 }
